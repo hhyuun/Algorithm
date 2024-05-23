d9e6200a2c0ad00ab2296d36e1b86d8124c80016
박현지 <119161420+hhyuun@users.noreply.github.com>
2024-05-24 02:01:12 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+
+        sc.close();
+
+        System.out.println(y-543);
+
+
+    }
+
+}
+
+
+
