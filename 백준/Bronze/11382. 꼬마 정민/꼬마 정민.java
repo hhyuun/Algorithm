1a6eb55c32bf1be16fdb25a8e05d4d955444f14c
박현지 <119161420+hhyuun@users.noreply.github.com>
2024-05-25 23:46:55 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        sc.close();
+
+        long sum = a + b + c;
+
+        System.out.println(sum);
+
+
+
+    }
+
+
+}
+
