acdc2e5d3acc544e1f57a79dd049c532d5b42723
박현지 <119161420+hhyuun@users.noreply.github.com>
2024-05-26 00:30:51 +0900

    [Bronze V] Title: 시험 성적, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        if(n>=90 && n<=100){
+            System.out.println("A");
+        }else if(n>=80){
+            System.out.println("B");
+        }else if(n>=70){
+            System.out.println("C");
+        }else if(n>=60){
+            System.out.println("D");
+        }else System.out.println("F");
+
+        sc.close();
+
+
+
+    }
+}
+
